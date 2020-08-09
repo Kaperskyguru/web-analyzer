7885e3b8e2d7012bc39a73761d98ad16aa1ef399
kaperskyguru <solomoneseme@gmail.com>
2020-08-09 03:48:46 +0100

    Fix deployment Bug

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-// const puppeteer = require("puppeteer");
+const puppeteer = require("puppeteer");
 const serveStatic = require("serve-static");
 const path = require("path");
 
@@ -11,71 +11,71 @@ app.use(serveStatic(path.join(__dirname, "../dist")));
 app.get("/ping", async (req, res) => {
   res.setHeader("Content-Type", "application/json");
   res.json({ test: "test" });
-  // try {
-  //   const browser = await puppeteer.launch({ args: ["--no-sandbox"] });
-  //   const page = await browser.newPage();
+  try {
+    const browser = await puppeteer.launch({ args: ["--no-sandbox"] });
+    const page = await browser.newPage();
 
-  //   //Important optimizations to get accurate results
-  //   await page.setCacheEnabled(false);
-  //   await page.setRequestInterception(true);
-  //   page.on("request", (request) => {
-  //     if (request.resourceType() === "document") {
-  //       request.continue();
-  //     } else {
-  //       request.abort();
-  //     }
-  //   });
-  //   let { url } = req.query;
+    //Important optimizations to get accurate results
+    await page.setCacheEnabled(false);
+    await page.setRequestInterception(true);
+    page.on("request", (request) => {
+      if (request.resourceType() === "document") {
+        request.continue();
+      } else {
+        request.abort();
+      }
+    });
+    let { url } = req.query;
 
-  //   await page.goto(url);
+    await page.goto(url);
 
-  //   const pref = await page.evaluate((url) => {
-  //     const { loadEventEnd, navigationStart } = performance.timing;
+    const pref = await page.evaluate((url) => {
+      const { loadEventEnd, navigationStart } = performance.timing;
 
-  //     let icon =
-  //       document.querySelector('link[rel="icon"]') ||
-  //       document.querySelector('link[rel="shortcut icon"]');
+      let icon =
+        document.querySelector('link[rel="icon"]') ||
+        document.querySelector('link[rel="shortcut icon"]');
 
-  //     let iconHref = "";
-  //     if (icon == null) {
-  //       iconHref = url + "/favicon.ico";
-  //     } else {
-  //       iconHref = icon.getAttribute("href");
-  //     }
+      let iconHref = "";
+      if (icon == null) {
+        iconHref = url + "/favicon.ico";
+      } else {
+        iconHref = icon.getAttribute("href");
+      }
 
-  //     return {
-  //       loadTime: loadEventEnd - navigationStart,
-  //       icon: iconHref,
-  //     };
-  //   }, url);
+      return {
+        loadTime: loadEventEnd - navigationStart,
+        icon: iconHref,
+      };
+    }, url);
 
-  //   await browser.close();
-  //   return res.json({
-  //     address: url,
-  //     result: pref.loadTime,
-  //     icon: pref.icon,
-  //     key: generateKey(),
-  //   });
-  // } catch (error) {
-  //   return res.json({
-  //     status: 500,
-  //     message: "Error occurred",
-  //     error: error,
-  //   });
-  // }
+    await browser.close();
+    return res.json({
+      address: url,
+      result: pref.loadTime,
+      icon: pref.icon,
+      key: generateKey(),
+    });
+  } catch (error) {
+    return res.json({
+      status: 500,
+      message: "Error occurred",
+      error: error,
+    });
+  }
 });
 
 // app.use("/static"));
 
-// app.get("/", function(req, res) {
-//   res.sendFile(path.join(__dirname, "../dist/index.html"));
-// });
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "../dist/index.html"));
+});
 
-// function generateKey() {
-//   return Math.random()
-//     .toString(36)
-//     .substring(5);
-// }
+function generateKey() {
+  return Math.random()
+    .toString(36)
+    .substring(5);
+}
 
 // const production = 'https://examplePage.com';
 // const development = 'http://localhost:3000/';
