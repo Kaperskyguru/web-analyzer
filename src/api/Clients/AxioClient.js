9e09e0890ec7eaf3ccf9393e117ebdb505bf0ba1
kaperskyguru <solomoneseme@gmail.com>
2020-08-09 04:03:24 +0100

    Fix deployment Bug

--- src/api/Clients/AxioClient.js
+++ src/api/Clients/AxioClient.js
@@ -1,41 +1,12 @@
 import axios from "axios";
+const production = window.location.origin;
+const development = "http://localhost:9000";
+const url = process.env.NODE_ENV == "production" ? production : development;
 
-// const PORT = process.env.PORT || 9000;
-// const HOST = process.env.HOST || "http://localhost";
+const baseURL = `${url}`;
 
-// const production = window.location.origin;
-// const development = "http://localhost:9000";
-// const url = process.env.NODE_ENV == "production" ? production : development;
-
-// console.log(process.env.NODE_ENV, url);
-// const baseURL = `${url}`;
-
-const httpsClient = axios.create();
-
-httpsClient.interceptors.request.use(
-  function(config) {
-    // Do something before request is sent
-    // console.log(config);
-    return config;
-  },
-  function(error) {
-    // Do something with request error
-    return Promise.reject(error);
-  }
-);
-
-// Add a response interceptor
-httpsClient.interceptors.response.use(
-  function(response) {
-    // Any status code that lie within the range of 2xx cause this function to trigger
-    // Do something with response data
-    return response;
-  },
-  function(error) {
-    // Any status codes that falls outside the range of 2xx cause this function to trigger
-    // Do something with response error
-    console.table(error);
-  }
-);
+const httpsClient = axios.create({
+  baseURL,
+});
 
 export default httpsClient;
