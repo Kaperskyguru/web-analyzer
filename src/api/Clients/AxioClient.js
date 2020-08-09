65536561f58f33fb95724a4573f67c09751bc51c
kaperskyguru <solomoneseme@gmail.com>
2020-08-09 02:57:38 +0100

    Fix deployment Bug

--- src/api/Clients/AxioClient.js
+++ src/api/Clients/AxioClient.js
@@ -3,41 +3,41 @@ import axios from "axios";
 // const PORT = process.env.PORT || 9000;
 // const HOST = process.env.HOST || "http://localhost";
 
-const production = window.location.origin;
-const development = "http://localhost:9000";
-const url = process.env.NODE_ENV == "production" ? production : development;
+// const production = window.location.origin;
+// const development = "http://localhost:9000";
+// const url = process.env.NODE_ENV == "production" ? production : development;
 
-console.log(process.env.NODE_ENV, url);
-const baseURL = `${url}`;
+// console.log(process.env.NODE_ENV, url);
+// const baseURL = `${url}`;
 
-const httpsClient = axios.create({
-  baseURL,
-});
+// const httpsClient = axios.create({
+//   baseURL,
+// });
 
-httpsClient.interceptors.request.use(
-  function(config) {
-    // Do something before request is sent
-    // console.log(config);
-    return config;
-  },
-  function(error) {
-    // Do something with request error
-    return Promise.reject(error);
-  }
-);
+// httpsClient.interceptors.request.use(
+//   function(config) {
+//     // Do something before request is sent
+//     // console.log(config);
+//     return config;
+//   },
+//   function(error) {
+//     // Do something with request error
+//     return Promise.reject(error);
+//   }
+// );
 
-// Add a response interceptor
-httpsClient.interceptors.response.use(
-  function(response) {
-    // Any status code that lie within the range of 2xx cause this function to trigger
-    // Do something with response data
-    return response;
-  },
-  function(error) {
-    // Any status codes that falls outside the range of 2xx cause this function to trigger
-    // Do something with response error
-    console.table(error);
-  }
-);
+// // Add a response interceptor
+// httpsClient.interceptors.response.use(
+//   function(response) {
+//     // Any status code that lie within the range of 2xx cause this function to trigger
+//     // Do something with response data
+//     return response;
+//   },
+//   function(error) {
+//     // Any status codes that falls outside the range of 2xx cause this function to trigger
+//     // Do something with response error
+//     console.table(error);
+//   }
+// );
 
-export default httpsClient;
+export default axios;
